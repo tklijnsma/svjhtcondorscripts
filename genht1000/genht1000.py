f1cd6927a8ebb411417a4d018e002ba33bba48b8
tklijnsm <tklijnsm@gmail.com>
2020-06-29 14:55:41 -0500

    running version

--- genht1000/genht1000.py
+++ genht1000/genht1000.py
@@ -1,5 +1,7 @@
 #$ pip module-install svjqondor
 #$ file cmssw_tarball root://cmseos.fnal.gov//store/user/klijnsma/semivis/cmssw_tarballs/CMSSW_10_2_18_htfilterprintout.tar.gz
+#$ htcondor request_memory 3000MB
+#$ htcondor request_disk 3000MB
 
 # Aim for the following events per job:
 # 150 : 400000, # ~105 evts/file,  110 jobs
@@ -7,40 +9,42 @@
 # 450 : 50000,  # ~400 evts/file,  30 jobs
 # 650 : 50000,  # ~550 evts/file,  30 jobs
 
+# For debugging:
+# $ htcondor hold True
+# $ set
+# $  mz 650
+# $  max_events 300
+# $  njobs 1
+# $  endset
+
+# Do the higher masses first
+#$ set
+#$  mz 450
+#$  max_events 50000
+#$  njobs 30
+#$  endset
 #$ set
 #$  mz 650
-#$  max_events 300
-#$  njobs 1
+#$  max_events 50000
+#$  njobs 30
 #$  endset
-
-# # Do the higher masses first
-# #$ set
-# #$  mz 450
-# #$  max_events 50000
-# #$  njobs 30
-# #$  endset
-# #$ set
-# #$  mz 650
-# #$  max_events 50000
-# #$  njobs 30
-# #$  endset
-# #$ set
-# #$  mz 150
-# #$  max_events 400000
-# #$  njobs 110
-# #$  endset
-# #$ set
-# #$  mz 250
-# #$  max_events 100000
-# #$  njobs 110
-# #$  endset
+#$ set
+#$  mz 150
+#$  max_events 400000
+#$  njobs 110
+#$  endset
+#$ set
+#$  mz 250
+#$  max_events 100000
+#$  njobs 110
+#$  endset
 
 import qondor, seutils, svjqondor, os.path as osp
 cmssw = qondor.init_cmssw()
 
 mz = int(qondor.get_var('mz'))
 max_events = int(qondor.get_var('max_events'))
-part = qondor.get_proc_id() + 1
+part = qondor.get_proc_id() + 1 # proc_id starts at 0, runSVJ expects starting at 1
 
 physics = {
     'year' : 2018,
@@ -59,6 +63,6 @@ expected_outfile = svjqondor.run_step_cmd(
 
 seutils.cp(
     expected_outfile,
-    'root://cmseos.fnal.gov//store/user/klijnsma/semivis/genht1000/test-Jun26_mz{mz}/{part}.root'
+    'root://cmseos.fnal.gov//store/user/klijnsma/semivis/genht1000/Jun29_mz{mz}/{part}.root'
     .format(mz=mz, part=part)
     )
